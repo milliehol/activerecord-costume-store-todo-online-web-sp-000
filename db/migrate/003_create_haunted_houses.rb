260e452265f76ef13dc15dc49b78f5cc24b54a6f
Carla Holcomb <carla.programmer@gmail.com>
2020-01-11 02:03:11 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,10 +7,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.integer :price
-      t.boolean :familyfriendly
-      t.string :opening_date
-      t.string :closing_date
-      t.string :long_description
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
     end
   end
 end
