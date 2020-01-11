0f54511c57e98aa153d0552bfbaba1a2968c89af
Carla Holcomb <carla.programmer@gmail.com>
2020-01-11 01:46:49 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :hauntedhouses do |t|
+      t.string :name
+      t.string :price
+      t.integer :size
+      t.string :image_url
+    end
+  end
+end
